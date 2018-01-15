d61641a2dcc8a59cedfca8416ff95043995f35ac
泽嘉 李 <romanzli@outlook.com>
2018-01-15 15:43:45 +0800

    制作成服务

--- QY.Quartz.Net.Console/Program.cs
+++ QY.Quartz.Net.Console/Program.cs
@@ -2,22 +2,29 @@
 using Quartz.Impl;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
 
 namespace QY.Quartz.Net.Console
 {
 
     class Program
     {
-        private static IScheduler scheduler;
         static void Main(string[] args)
         {
-            ISchedulerFactory schedulerFactory = new StdSchedulerFactory();
-            scheduler = schedulerFactory.GetScheduler();
+            HostFactory.Run(x =>
+            {
+                x.Service<WindowsService>();
+                x.RunAsLocalSystem();
+                x.SetDescription("QuartzDemo服务描述");
+                x.SetDisplayName("QuartzDemo服务显示名称");
+                x.SetServiceName("QuartzDemo服务名称");
 
-            scheduler.Start();
+                x.EnablePauseAndContinue();
+            });
         }
     }
 }
