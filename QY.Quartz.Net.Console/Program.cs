a48fbddd2f089767ad5ed8cdb11c631b882d4828
泽嘉 李 <romanzli@outlook.com>
2018-01-15 16:08:30 +0800

    注册服务信息改成配置

--- QY.Quartz.Net.Console/Program.cs
+++ QY.Quartz.Net.Console/Program.cs
@@ -2,6 +2,7 @@
 using Quartz.Impl;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -15,13 +16,17 @@ namespace QY.Quartz.Net.Console
     {
         static void Main(string[] args)
         {
+            string servicename = ConfigurationManager.AppSettings["WindowsServiceName"];
+            string displayname = ConfigurationManager.AppSettings["WindowsServiceDisplayName"];
+            string sescription = ConfigurationManager.AppSettings["WindowsServiceDescription"];
             HostFactory.Run(x =>
             {
                 x.Service<WindowsService>();
                 x.RunAsLocalSystem();
-                x.SetDescription("QuartzDemo服务描述");
-                x.SetDisplayName("QuartzDemo服务显示名称");
-                x.SetServiceName("QuartzDemo服务名称");
+
+                x.SetServiceName(servicename);
+                x.SetDisplayName(displayname);
+                x.SetDescription(sescription);
 
                 x.EnablePauseAndContinue();
             });
